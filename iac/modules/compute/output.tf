c7cc8f8e8cf9baff3a8afe88c98ea542bcf62728
NearTouchES <estoque@pucp.edu.pe>
2025-07-04 18:10:48 -0500

    Actualización final v9

--- iac/modules/compute/output.tf
+++ iac/modules/compute/output.tf
@@ -1,11 +1,11 @@
 output "nombre_cluster" {
-  value = aws_ecs_cluster.cluster_tienda_servicio.name
+  value = aws_ecs_cluster.cluster_tiendaservicio.name
 }
 
 output "task_definition_arn" {
-  value = aws_ecs_task_definition.definicion_tarea_tienda_servicio.arn
+  value = aws_ecs_task_definition.definicion_tarea_tiendaservicio.arn
 }
 
 output "load_balancer_url" {
-  value = aws_lb.tienda_servicio_load_balancer.dns_name
+  value = aws_lb.tiendaservicio_load_balancer.dns_name
 }
