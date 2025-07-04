78018b6b3c1ea5b0a7c7a2df11c7bd59aa5ef75b
NearTouchES <estoque@pucp.edu.pe>
2025-07-04 06:19:27 -0500

    Actualiza variables y configuración Terraform

--- iac/main.tf
+++ iac/main.tf
@@ -8,34 +8,35 @@ provider "aws" {
 }
 
 module "serverless" {
-    source = "./modules/serverless"
-    rol_lambda_arn = var.rol_lab_arn
-    url_base_servicio = "http://${module.compute.load_balancer_url}/api"
+  source              = "./modules/serverless"
+  rol_lambda_arn      = var.rol_lab_arn
+  url_base_servicio   = "http://${module.compute.load_balancer_url}/api"
+  crear_orden_path    = "${path.root}/../serverless/tiendaServicio/packages/funciones/crear-orden/build"
 }
 
 module "compute" {
-    source = "./modules/compute"
-    nombre_cluster = var.nombre_cluster_ecs
-    familia_tarea = var.familia_tarea_ecs
-    rol_lab_arn = var.rol_lab_arn
-    id_cuenta_aws = var.id_cuenta_aws
-    region_aws = var.region
-    nombre_repo_ecr = var.nombre_repo_ecr
-    servidor_base_datos = var.servidor_base_datos
-    usuario_base_datos = var.usuario_base_datos
-    contrasenha_base_datos = var.contrasenha_base_datos
-    nombre_servicio_ecs = var.nombre_servicio_ecs
+  source                  = "./modules/compute"
+  nombre_cluster          = var.nombre_cluster_ecs
+  familia_tarea           = var.familia_tarea_ecs
+  rol_lab_arn             = var.rol_lab_arn
+  id_cuenta_aws           = var.id_cuenta_aws
+  region_aws              = var.region
+  nombre_repo_ecr         = var.nombre_repo_ecr
+  servidor_base_datos     = var.servidor_base_datos
+  usuario_base_datos      = var.usuario_base_datos
+  contrasenha_base_datos  = var.contrasenha_base_datos
+  nombre_servicio_ecs     = var.nombre_servicio_ecs
 }
 
 module "events" {
-    source = "./modules/events"
-    crear_orden_funcion_arn = module.serverless.crear_orden_funcion_arn
-    crear_orden_funcion_name = module.serverless.crear_orden_funcion_name
+  source                    = "./modules/events"
+  crear_orden_funcion_arn   = module.serverless.crear_orden_funcion_arn
+  crear_orden_funcion_name  = module.serverless.crear_orden_funcion_name
 }
 
 module "api" {
-    source = "./modules/api"
-    load_balancer_url = module.compute.load_balancer_url
-    rol_lab_arn = var.rol_lab_arn
-    event_bus_name = module.events.event_bus_name
+  source           = "./modules/api"
+  load_balancer_url = module.compute.load_balancer_url
+  rol_lab_arn       = var.rol_lab_arn
+  event_bus_name    = module.events.event_bus_name
 }
--- iac/variables.tf
+++ iac/variables.tf
@@ -1,5 +1,6 @@
 variable "region" {
-  description = "Region en la que se desplegarán los recursos de AWS"
+  description = "Región en la que se desplegarán los recursos de AWS"
+  type        = string
   default     = "us-east-1"
 }
 
@@ -9,7 +10,7 @@ variable "id_cuenta_aws" {
 }
 
 variable "rol_lab_arn" {
-  description = "ARN del rol del laboratorio en AWS academy"
+  description = "ARN del rol de laboratorio (por ejemplo, AWS Academy)"
   type        = string
 }
 
@@ -19,35 +20,37 @@ variable "url_base_servicio" {
 }
 
 variable "nombre_cluster_ecs" {
-    description = "Nombre del clúster ECS donde se desplegará la tarea"
-    type        = string
+  description = "Nombre del clúster ECS donde se desplegará la tarea"
+  type        = string
 }
+
 variable "familia_tarea_ecs" {
-    description = "value de la familia de tareas ECS"
-    type        = string
+  description = "Nombre de la familia de tareas ECS"
+  type        = string
 }
 
 variable "nombre_repo_ecr" {
-    description = "value del repositorio ECR donde se almacenará la imagen del contenedor"
-    type        = string
+  description = "Nombre del repositorio ECR donde se almacenará la imagen del contenedor"
+  type        = string
 }
 
 variable "servidor_base_datos" {
-    description = "value de la URL de la base de datos para la aplicación"
-    type        = string
+  description = "Dirección del servidor de la base de datos (RDS u otro)"
+  type        = string
 }
 
 variable "usuario_base_datos" {
-    description = "value del usuario de la base de datos para la aplicación"
-    type        = string
+  description = "Usuario de la base de datos para la aplicación"
+  type        = string
 }
 
 variable "contrasenha_base_datos" {
-    description = "value de la contraseña de la base de datos para la aplicación"
-    type        = string
+  description = "Contraseña del usuario de la base de datos para la aplicación"
+  type        = string
+  sensitive   = true
 }
 
 variable "nombre_servicio_ecs" {
-    description = "Nombre del servicio ECS donde se desplegará la tarea"
-    type        = string
+  description = "Nombre del servicio ECS donde se desplegará la tarea"
+  type        = string
 }
