760ca9de7f7963936ad02c12f4bd108266d36b91
NearTouchES <estoque@pucp.edu.pe>
2025-07-04 06:51:36 -0500

    fix: remover argumento no soportado en módulo serverless

--- iac/main.tf
+++ iac/main.tf
@@ -11,7 +11,6 @@ module "serverless" {
   source = "./modules/serverless"
   rol_lambda_arn = var.rol_lab_arn
   url_base_servicio = "http://${module.compute.load_balancer_url}/api"
-  crear_orden_path = "${path.root}/../serverless/tiendaServicio/packages/funciones/crear-orden/build"
 }
 
 module "compute" {
