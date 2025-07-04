e6e1924a4e7d749aea8c24927110ecf5abae1310
NearTouchES <estoque@pucp.edu.pe>
2025-07-04 18:58:51 -0500

    Actualización final v12

---
+++ frontend/app/ventas/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import PaginaVentas from "@/componentes/PaginaVentas";
+
+export default function PaginaVentasApp() {
+  return (
+    <div>
+      <PaginaVentas />
+    </div>
+  );
+}
