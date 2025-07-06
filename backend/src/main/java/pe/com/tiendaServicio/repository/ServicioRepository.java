2192e852b365e35c29e36e698b04102326816cfc
NearTouchES <estoque@pucp.edu.pe>
2025-07-06 16:45:32 -0500

    servicio2

--- backend/src/main/java/pe/com/tiendaServicio/repository/ServicioRepository.java
+++ backend/src/main/java/pe/com/tiendaServicio/repository/ServicioRepository.java
@@ -2,7 +2,7 @@ package pe.com.tiendaServicio.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import pe.com.tiendaServicio.model.Servicio;
+import pe.com.tiendaServicio.model.Servicios;
 
 @Repository
 public interface ServicioRepository extends JpaRepository<Servicio, Integer> {
