2a21eeee398dcb18790f437c508d76f783c42d06
NearTouchES <estoque@pucp.edu.pe>
2025-07-06 13:18:02 -0500

    tienda

---
+++ backend/src/main/java/pe/com/tiendaServicio/TiendaServicioApp.java
@@ -0,0 +1,12 @@
+package pe.com.tiendaServicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaServicioApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaServicioApp.class, args);
+    }
+}
